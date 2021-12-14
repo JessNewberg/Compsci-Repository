0f22e1756b0102721d42c600a42a681c0d5b83c3
Jess <95761393+JessNewberg@users.noreply.github.com>
2021-12-14 09:23:44 -0500

    UPDATED Converters

--- Bin_to_Int.py
+++ Bin_to_Int.py
@@ -1,19 +1,16 @@
-def bin_to_int (a:str):
-    response = 0
-    if a[0]: == 1:
-        response + 129
-    if a[1]: == 1:
-        response + 65
-    if a[2]: == 1:
-        response + 33
-    if a[3] == 1:
-        response + 17
-    if a[4] == 1:
-        response + 9
-    if a[5]: == 1:
-        response + 5
-    if a[6]: == 1:
-        response + 3
-    if a[7] == 1:
-        response + 1
-    return (response)
+"""this code converts 8 bits of binary to an integer."""
+def bin_to_int(userinput:str):
+    """sets up our code. we will begin by setting our number equal to 0, and saying that we
+    start by observing the first digit of the binary (the first power
+    of two, 2^0). Also defines length of the user's input."""
+    converted = 0
+    poweroftwo = 1
+    i = len(userinput)
+
+    while i>=0:
+        """the number is set equal to its previous number plus either the power of two
+        we are on if the digit of the binary is 1, or just the number if it is not."""
+        converted += (poweroftwo * (converted[i]))
+        poweroftwo * 2
+        i = i- 1
+    return converted
--- Int_to_Bin1.py
+++ Int_to_Bin1.py
@@ -1,9 +1,18 @@
+""" this code converts an integer of any length to binary. """
 def Int_to_Bin(num):
     converted =''
     if num==0:
         converted='0'+ converted
+        """When the number is , a 0 is added and the code moves onto
+            the next character of the binary. """
     else:
         while num >0:
             converted = str(num%2)+converted
             num=num//2
+            """While a number isnt equal to 0, it keeps being divided by 2. Every time it is divisible, a 1 is added."""
     print (converted)
+""" this is the only way to get this to work in Visual Studio. Haven't quite figured it out yet."""
+def main():
+    Int_to_Bin (10)
+
+main()
